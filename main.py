fa7d5944a0ead3f3605d83ff38c0a0bbfd4edcbc
Evg Fomin <Couguar72@gmail.com>
2025-05-05 00:13:45 +0300

    добавлены новые функции, обновлён файл main

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-# Импорт функций маскировки из файла masks
-from src.masks import get_mask_account, get_mask_card_number
+# Импорт функций маскировки из файла masks или widget
+# from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date, mask_account_card
 
 # Запрос номера карты клиента и возврат замаскированного номера
-card_number = input("Введите номер карты: \n")
-print(get_mask_card_number(card_number))
+# card_number = input("Введите номер карты: \n")
+# print(get_mask_card_number(card_number))
 
 # Запрос счета карты клиента и возврат замаскированного счета
-account_number = input("Введите номер счета: \n")
-print(get_mask_account(account_number))
+# account_number = input("Введите номер счета: \n")
+# print(get_mask_account(account_number))
+
+# Запрос данных счёта или карты клиента и возврат замаскированных данных
+account_card = input("Введите данные: \n")
+print(mask_account_card(account_card))
+
+# Запрос даты и возврат в удобном формате
+user_date = input("Введите дату: \n")
+print(get_date(user_date))
--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,17 @@
+# Импорт функций маскировки из файла masks
+from src.masks import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(account_card: str) -> str:
-    pass
+    """Функция маскирует номер счета или карты в зависимости от предоставленных данных"""
+    number_account_card = "".join(filter(str.isdigit, account_card))
+    if len(number_account_card) > 16:
+        return f"{account_card[:-21]} {get_mask_account(number_account_card)}"
+    else:
+        return f"{account_card[:-17]} {get_mask_card_number(number_account_card)}"
 
 
+# Функция преобразования даты
 def get_date(user_date: str) -> str:
-    pass
+    """Функция преобразовывает и выводит получаемую дату в формате ДД.ММ.ГГГГ"""
+    return f"{user_date[8:10]}.{user_date[5:7]}.{user_date[0:4]}"
